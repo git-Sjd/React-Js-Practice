641922685040e026059b6d8bc283061116e1f4f7
git-Sjd <sajid.tisl@gmail.com>
2023-06-04 11:18:56 +0530

    UI Changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 
+import React, { useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 
+
 function App() {
+  const [mode, setMode] = useState("light")
   return (
     <>
-      <Navbar title="TextUtils" home="Home" />
+      <Navbar title="TextUtils" home="Home" mode={mode} />
       <div className="container">
         <TextForm heading="Please Enter your text in below box" />
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar(props) {
     return (
         <div>
 
-            <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+            <nav className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode}`}>
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">{props.title}</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
